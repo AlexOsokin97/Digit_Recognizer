3fe837fe4eba0c6cfc60c309e6a467498ac778f4
Alexander Osokin <60130537+AlexOsokin97@users.noreply.github.com>
2020-08-08 02:29:22 +0300

    Update code_testing.py

--- code_testing.py
+++ code_testing.py
@@ -7,30 +7,48 @@ Created on Fri Aug  7 21:41:40 2020
 
 import pandas as pd
 import numpy as np
+from PIL import Image
 
 data = pd.read_csv('train.csv')
 
-zeros = data.loc[data['label'] == 0]
+zeros = data.loc[data['label'] == 0].copy()
 zeros.drop(['label'], axis=1, inplace=True)
+zeros.reset_index(drop=True, inplace=True)
 
-cols = zeros.columns
+ones = data.loc[data['label'] == 1].copy()
+ones.drop(['label'], axis=1, inplace=True)
+ones.reset_index(drop=True, inplace=True)
 
-zero1 = zeros.iloc[[1], 1:]
+cols = ones.columns.tolist()
 
-image = []
+one1 = ones.iloc[[1], :].copy()
+
+one1_pixls = []
 i = 0
-col_list = zero1.columns
+col_list = one1.columns.tolist()
 
 while i < 28:
     row = []
     counter = 0
     for column in col_list:
-        val = zero1[column].values[0]
+        val = one1[column].values[0]
         row.append(val)
         counter += 1
         col_list.remove(column)
         if counter == 27:
             break
-    image.append(row)
+    one1_pixls.append(row)
     i += 1
+
+one1_pixls = np.array(one1_pixls, dtype=np.uint8)
+image = Image.fromarray(one1_pixls)
+image.save('new.png')
+
+
+
+
+
+
+
+
 
