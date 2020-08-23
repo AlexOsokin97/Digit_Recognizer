9887fcc25c85c3af715f6ed9d6330e1b4443759f
Alexander Osokin <60130537+AlexOsokin97@users.noreply.github.com>
2020-08-23 16:52:59 +0300

    pew

--- FlaskAPI/app.py
+++ FlaskAPI/app.py
@@ -1,38 +1,13 @@
 import flask
-from flask import Flask, jsonify, jsonify, request
-import json
-import numpy as np
-import pickle
-from http.server import HTTPServer, SimpleHTTPRequestHandler, test
-import sys
+from flask_cors import CORS
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+CORS(app)
 
-class CORSRequestHandler (SimpleHTTPRequestHandler):
-    def end_headers (self):
-        self.send_header('Access-Control-Allow-Origin', '*')
-        SimpleHTTPRequestHandler.end_headers(self)
 
 @app.route('/guess', methods=['POST'])
-def guess():
-    #stub input features
-    image = request.files['image']  
-    #x = request_json['input']
-    #x_in = np.array(x).reshape(1, -1)
-    #load model & make a guess
-    #model = load_models()
-    #guessing = model.predict(x_in)[0]
-    #send a response
-    response = json.dumps({'response': image})
-    return response, 200
+def home():
+    return '1'
 
-def load_models():
-    file_name = 'model_path'
-    with open(file_name, 'rb') as pickled:
-        data = pickle.load(pickled)
-        model = data['model']
-    return model
-
-if __name__ == '__main__':
-    test(CORSRequestHandler, HTTPServer, port=int(sys.argv[1]) if len(sys.argv) > 1 else 8000)
-    app.run(debug=True)
+app.run()
